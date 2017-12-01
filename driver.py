158c614219b5abc32ac28fbea2eda8f4b11310a4
sekaranglila <sekaranglilah@gmail.com>
2017-12-02 02:24:37 +0700

    add evaluation calculation

--- driver.py
+++ driver.py
@@ -3,6 +3,13 @@ import utils
 import pickle
 import numpy as np
 from sklearn.metrics import adjusted_rand_score
+from sklearn.metrics import adjusted_mutual_info_score
+from sklearn.metrics import homogeneity_score
+from sklearn.metrics import completeness_score
+from sklearn.metrics import v_measure_score
+from sklearn.metrics import fowlkes_mallows_score
+from sklearn.metrics import silhouette_score
+from sklearn.metrics import calinski_harabaz_score
 import json
 
 data = [
@@ -26,16 +33,23 @@ classifier = KMeans(n_clusters= 2)
 
 classifier.train(input_data=dataset,normalize="normalize")
 
+# Adjusted Rand Index
+print("------------------------ K-MEANS MODEL --------------------------------")
 list_clustered = classifier.label_clustered
 print("ARI SCORE: " + str(adjusted_rand_score(np.array(list_label), np.array(list_clustered))))
-print("Model:")
-print(classifier.cluster_membership)
-print(classifier.centroids)
+print("MUTUAL INFO SCORE: " + str(adjusted_mutual_info_score(np.array(list_label), np.array(list_clustered))))
+print("HOMOGENEITY SCORE: " + str(homogeneity_score(np.array(list_label), np.array(list_clustered))))
+print("COMPLETENESS SCORE: " + str(completeness_score(np.array(list_label), np.array(list_clustered))))
+print("V MEASURE SCORE: " + str(v_measure_score(np.array(list_label), np.array(list_clustered))))
+# np.seterr(all='warn')
+# np.multiply.reduce(np.arange(21)+1)
+print("FOWLKES-MALLOWS SCORE: " + str(fowlkes_mallows_score(np.array(list_label), np.array(list_clustered))))
+# print("SILHOUETTE SCORE: " + str(silhouette_score(np.array(dataset), np.array(list_label), metric="euclidean")))
+print("CALINSKI-HARABAZ SCORE: " + str(calinski_harabaz_score(np.array(dataset), np.array(list_label))))
+
+
+# print("Model:")
+# print(classifier.cluster_membership)
+# print(classifier.centroids)
+
 pickle.dump(classifier, file=open("kmeans_model.ler","wb"))
-
-# import pdb
-# pdb.set_trace()
-
-# output = json.dumps(classifier.__dict__)
-
-# print(output, file=open("model.json","w"))
